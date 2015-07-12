1a11dec483ba9758b971060f2513121ee7bd93b4
Jacky <875636632@qq.com>
2015-07-12 22:02:32 +0800

    新增数据帮助类

---
+++ src/model/Province.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Province {
+
+	private int id;
+	private String privinceName;
+	private String provinceCode;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPrivinceName() {
+		return privinceName;
+	}
+	public void setProvinceName(String privinceName) {
+		this.privinceName = privinceName;
+	}
+	public String getProvinceCode() {
+		return provinceCode;
+	}
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+	
+}
